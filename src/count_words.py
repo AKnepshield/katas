9a916bbd81c87b5190f9e8f18fd0d77b6a798fe5
Charles Knepshield <aknepshield@gmail.com>
2024-08-16 12:13:32 -0500

    todays second attempt

---
+++ src/count_words.py
@@ -0,0 +1,4 @@
+def count_words(parsed_string):
+    list_of_strings = parsed_string.split()
+    length_of_string = len(list_of_strings)
+    return length_of_string
---
+++ tests/test_count_words.py
@@ -0,0 +1,19 @@
+from src.count_words import count_words
+
+
+def test_count_zero_words():
+    word_count = ""
+    word_count_return = count_words(word_count)
+    assert word_count_return == 0
+
+
+def test_count_one_words():
+    word_count = "word"
+    word_count_return = count_words(word_count)
+    assert word_count_return == 1
+
+
+def test_count_two_words():
+    word_count = "two words"
+    word_count_return = count_words(word_count)
+    assert word_count_return == 2
