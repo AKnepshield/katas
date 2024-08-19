01f88fc8066bcaf9280b4d5ca40b185944ae48f0
Charles Knepshield <aknepshield@gmail.com>
2024-08-19 12:17:52 -0500

    record collection, second go

---
+++ tests/test_record_collection.py
@@ -0,0 +1,11 @@
+from src.record_collection import RecordCollection
+
+
+def test_add_record():
+    collection = RecordCollection()
+    title = "Hear Nothing See Nothing Say Nothing"
+    artist = "Discharge"
+    collection.add_record(title, artist)
+    assert collection.records == [
+        {"title": "Hear Nothing See Nothing Say Nothing", "artist": "Discharge"}
+    ]
