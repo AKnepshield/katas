701d7ca6aa4847946faba66e87e86c5c1c8f1159
Charles Knepshield <aknepshield@gmail.com>
2024-08-19 11:58:19 -0500

    It's actually 8-19 but added add record function and test

---
+++ src/count_words.py
@@ -0,0 +1,4 @@
+def count_words(parsed_list: int):
+    string_list = parsed_list.split()
+    string_count = len(string_list)
+    return string_count
---
+++ tests/test_count_words.py
@@ -0,0 +1,7 @@
+# from src.count_words import count_words
+
+
+# def test_count_words():
+#     word_count = ""
+#     count_words_int = count_words(word_count)
+#     assert count_words_int == 0
