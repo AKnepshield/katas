fd95c7350c2acc2234ee41faec73e09d2dafcb8f
Charles Knepshield <aknepshield@gmail.com>
2024-08-24 15:21:49 -0500

    add record, get record by title, get record collection, remove record by title, count records, list artists methods added

---
+++ tests/test_record_collection.py
@@ -0,0 +1,85 @@
+from src.record_collection import RecordCollection
+
+
+def test_add_record():
+    collection = RecordCollection()
+
+    record = collection.add_record("Peace & Security", "Death Threat")
+
+    expected_record = {"title": "Peace & Security", "artist": "Death Threat"}
+
+    assert expected_record == record
+
+
+def test_get_record_by_title():
+    collection = RecordCollection()
+    record = collection.add_record("Peace & Security", "Death Threat")
+    returned_record = collection.get_record_by_title("Peace & Security")
+    expected_record = {"title": "Peace & Security", "artist": "Death Threat"}
+    assert expected_record == returned_record
+
+
+def test_get_record_collection():
+    collection = RecordCollection()
+    collection.add_record("Peace & Security", "Death Threat")
+    collection.add_record("Big Kiss Goodnight", "Trapped Under Ice")
+
+    returned_collection = collection.get_record_collection()
+
+    expected_returned_collection = [
+        {"title": "Peace & Security", "artist": "Death Threat"},
+        {"title": "Big Kiss Goodnight", "artist": "Trapped Under Ice"},
+    ]
+
+    assert returned_collection == expected_returned_collection
+
+
+def test_remove_record_by_title():
+    collection = RecordCollection()
+    collection.add_record("Peace & Security", "Death Threat")
+    collection.add_record("Big Kiss Goodnight", "Trapped Under Ice")
+    collection.add_record("Heatwave", "Trapped Under Ice")
+    collection.add_record("World Be Free", "World Be Free")
+
+    removed_record = collection.remove_record_by_title("Peace & Security")
+    current_collection = collection.get_record_collection()
+
+    expected_removed_record = {"title": "Peace & Security", "artist": "Death Threat"}
+
+    expected_collection_without_removed_record = [
+        {"title": "Big Kiss Goodnight", "artist": "Trapped Under Ice"},
+        {"title": "Heatwave", "artist": "Trapped Under Ice"},
+        {"title": "World Be Free", "artist": "World Be Free"},
+    ]
+
+    assert removed_record == expected_removed_record
+    assert expected_collection_without_removed_record == current_collection
+
+
+def test_count_records():
+    collection = RecordCollection()
+    collection.add_record("Peace & Security", "Death Threat")
+    collection.add_record("Big Kiss Goodnight", "Trapped Under Ice")
+    collection.add_record("Heatwave", "Trapped Under Ice")
+    collection.add_record("World Be Free", "World Be Free")
+
+    record_count = collection.count_records()
+
+    expected_count = 4
+
+    assert expected_count == record_count
+
+
+def test_list_artists():
+
+    collection = RecordCollection()
+    collection.add_record("Peace & Security", "Death Threat")
+    collection.add_record("Big Kiss Goodnight", "Trapped Under Ice")
+    collection.add_record("Heatwave", "Trapped Under Ice")
+    collection.add_record("World Be Free", "World Be Free")
+
+    list_of_artists = collection.list_artists()
+
+    expected_list_of_artists = ["Death Threat", "Trapped Under Ice", "World Be Free"]
+
+    assert sorted(expected_list_of_artists) == sorted(list_of_artists)
