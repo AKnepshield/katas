7598957b3e54ba1d10d36febe720f4cfa16fce0b
Charles Knepshield <aknepshield@gmail.com>
2024-08-25 12:56:39 -0500

    added add record, get record by title, get collection, remove record by title

---
+++ src/record_collection.py
@@ -0,0 +1,23 @@
+class RecordCollection:
+    def __init__(self):
+        self.record_collection = []
+
+    def add_record(self, artist, title):
+        record = {"artist": artist, "title": title}
+        self.record_collection.append(record)
+        return record
+
+    def get_record_by_title(self, title):
+        for record in self.record_collection:
+            if record["title"] == title:
+                return record
+        return None
+
+    def get_record_collection(self):
+        return self.record_collection
+
+    def remove_record_by_title(self, title):
+        for record in self.record_collection:
+            if record["title"] == title:
+                self.record_collection.remove(record)
+                return self.record_collection
---
+++ tests/test_record_collection.py
@@ -0,0 +1,61 @@
+from src.record_collection import RecordCollection
+
+
+def test_add_record():
+    collection = RecordCollection()
+    record = collection.add_record("Misfits", "Hybrid Moments")
+    expected_record = {"artist": "Misfits", "title": "Hybrid Moments"}
+    assert expected_record == record
+
+
+def test_get_record():
+    collection = RecordCollection()
+    record = collection.add_record("Misfits", "Hybrid Moments")
+    collection.get_record_by_title("Hybrid Moments")
+    expected_record = {"artist": "Misfits", "title": "Hybrid Moments"}
+    assert expected_record == record
+
+
+def test_get_whole_collection():
+    collection = RecordCollection()
+    collection.add_record("Misfits", "Hybrid Moments")
+    collection.add_record("Misfits", "Earth AD")
+    collection.add_record("Danzig", "Danzig I")
+    collection.add_record("Samhain", "Initium")
+
+    current_collection = collection.get_record_collection()
+
+    expected_collection = [
+        {"artist": "Misfits", "title": "Hybrid Moments"},
+        {"artist": "Misfits", "title": "Earth AD"},
+        {"artist": "Danzig", "title": "Danzig I"},
+        {"artist": "Samhain", "title": "Initium"},
+    ]
+
+    assert expected_collection == current_collection
+
+
+def test_remove_record_by_title():
+    collection = RecordCollection()
+    collection.add_record("Misfits", "Hybrid Moments")
+    collection.add_record("Misfits", "Earth AD")
+    collection.add_record("Danzig", "Danzig I")
+    collection.add_record("Samhain", "Initium")
+
+    current_collection = [
+        {"artist": "Misfits", "title": "Hybrid Moments"},
+        {"artist": "Misfits", "title": "Earth AD"},
+        {"artist": "Danzig", "title": "Danzig I"},
+        {"artist": "Samhain", "title": "Initium"},
+    ]
+    removed_record = collection.remove_record_by_title("Earth AD")
+    collection_without_removed_record = collection.get_record_collection()
+    expected_removed_record = {"artist": "Misfits", "title": "Earth AD"}
+
+    expected_collection = [
+        {"artist": "Misfits", "title": "Hybrid Moments"},
+        {"artist": "Danzig", "title": "Danzig I"},
+        {"artist": "Samhain", "title": "Initium"},
+    ]
+
+    assert expected_collection == collection_without_removed_record
