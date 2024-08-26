8fa3aa8ed6f44d89740606cad8b20ecc5fc88a7d
Charles Knepshield <aknepshield@gmail.com>
2024-08-26 11:19:28 -0500

    added get record by title method

--- src/record_collection.py
+++ src/record_collection.py
@@ -6,3 +6,9 @@ class RecordCollection:
         record = {"artist": artist, "title": title}
         self.record_collection.append(record)
         return record
+
+    def get_record_by_title(self, title):
+        for record in self.record_collection:
+            if record["title"] == title:
+                return record
+        return None
--- tests/test_record_collection.py
+++ tests/test_record_collection.py
@@ -6,3 +6,14 @@ def test_add_record():
     record = collection.add_record("The Smiths", "Louder Than Bombs")
     expected_record = {"artist": "The Smiths", "title": "Louder Than Bombs"}
     assert expected_record == record
+
+
+def test_get_record_by_title():
+    # Arrange
+    collection = RecordCollection()
+    collection.add_record("The Smiths", "Hatful Of Hollow")
+    # Act
+    retrieved_record = collection.get_record_by_title("Hatful Of Hollow")
+    expected_record = {"artist": "The Smiths", "title": "Hatful Of Hollow"}
+    # Assert
+    assert retrieved_record == expected_record
