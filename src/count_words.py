e8b3ec1ac66c85ccac57ee9012e12cdc77fcca7c
Charles Knepshield <aknepshield@gmail.com>
2024-09-02 13:54:19 -0500

    count_words kata

--- src/count_words.py
+++ src/count_words.py
@@ -1,20 +1,3 @@
-def count_words(words):
-    words_list = []
-    for word in words:
-        words_list.append(word)
-    word_count = len(words_list)
+def count_words(sentence):
+    word_count = sentence.split()
     return word_count
-
-
-words = ["red", "black", "green"]
-word_count = count_words(words)
-print({word_count})
-
-
-def count_words(words):
-    return len(words)
-
-
-words = ["red", "black", "green"]
-word_count = count_words(words)
-print(f"{word_count}")
