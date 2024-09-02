f1c21fb8e02476a60c13b7a80f3f157848ce1bc0
Charles Knepshield <aknepshield@gmail.com>
2024-09-02 14:11:34 -0500

    Did a thing

--- src/run_record_collection.py
+++ src/run_record_collection.py
@@ -6,7 +6,8 @@ def run(in_mem_collection, json_manager):
     # 8
     keep_running = True
     # 8.5 - Checks if keep_running is True
-    while keep_running:
+    # if keep_running == True
+    while keep_running == True:
         # 9
         keep_running = view_or_create_owners_menu(in_mem_collection, json_manager)
         # 16 - saving value to any_name
