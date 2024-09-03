4b4f147953b2fd290c0fe55da57b75c4595dd78b
Charles Knepshield <aknepshield@gmail.com>
2024-09-03 12:38:41 -0500

    committing list on main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    print("A Thing")
+# Had a menu
+# Had option to create new owner using 1
+# Had option to view current owners using 2
+# Had option to exit application using 3
+# Captured user input for options
+# Captured user input for new owner name if they choose 1
+# Viewed list of current users, including new owner if they chose 2
+#
+# If they choose 3, application ceased to function
+# Created a "if" statement for application to run
+#
+# Created a setup function
+# Created a run function
+# Created a "while" loop inside of run function
+# Inside of while loop, wrote "if" functions that returned True so that it could continue to run
+# For option 3, exit the applcation, it returned False in order to exit the application
+#
